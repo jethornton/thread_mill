2d77cd18e85edf84ed81d5eb84a5ab57452657e0
John Thornton <jthornton@gnipsel.com>
2019-04-09 07:17:21 -0500

    get gcode going

--- thread_mill/mainwindow.py
+++ thread_mill/mainwindow.py
@@ -11,6 +11,7 @@ from math import sqrt as sqrt
 
 from PyQt5.QtSql import QSqlDatabase, QSqlQueryModel, QSqlQuery
 from PyQt5.QtWidgets import QDataWidgetMapper
+from PyQt5.Qt import QClipboard
 
 class MyMainWindow(VCPMainWindow):
     """Main window class for the VCP."""
@@ -27,9 +28,14 @@ class MyMainWindow(VCPMainWindow):
         self.sptmSizeBackBtn.clicked.connect(self.sptmSizeBack)
         self.drillSizeFwdBtn.clicked.connect(self.drillSizeFwd)
         self.drillSizeBackBtn.clicked.connect(self.drillSizeBack)
-        self.testBtn.clicked.connect(self.testSql)
+        self.numPassesSP.valueChanged.connect(self.numPassesCalc)
+        self.holeDiaSb.valueChanged.connect(self.holeDiaCalc)
+        self.linearFeedSb.valueChanged.connect(self.linearFeedCalc)
+        self.genGcodeBtn.clicked.connect(self.genGcode)
+
         self.testFwdBtn.clicked.connect(self.testFwd)
         self.testBackBtn.clicked.connect(self.testBack)
+        self.saveFilePb.clicked.connect(self.saveFile)
 
 
         if not self.open_db():
@@ -37,7 +43,7 @@ class MyMainWindow(VCPMainWindow):
 
         self.sptmSizeInit()
         self.threadFormInit()
-
+        self.numPassesCalc()
 
     def open_db(self):
         db = QSqlDatabase.addDatabase('QSQLITE')
@@ -110,7 +116,7 @@ class MyMainWindow(VCPMainWindow):
         self.sizeModel.setQuery(sizeSelect)
         self.sizeMapper.setModel(self.sizeModel)
         self.sizeMapper.addMapping(self.threadSizeLbl, 0, b'text')
-        self.sizeMapper.addMapping(self.threadPitchLbl, 1, b'text')
+        self.sizeMapper.addMapping(self.threadTPILbl, 1, b'text')
         self.sizeMapper.addMapping(self.threadMajorDiaLbl, 2, b'text')
         self.sizeMapper.addMapping(self.minMajorDiaLbl, 3, b'text')
         self.sizeMapper.addMapping(self.maxMinorDiaLbl, 4, b'text')
@@ -122,7 +128,7 @@ class MyMainWindow(VCPMainWindow):
         self.sizeMapper.setCurrentIndex(index)
         self.drillSizeInit()
         self.threadSizeCalc()
-
+        self.numPassesCalc()
 
     def threadSizeFwd(self):
         if self.sizeMapper.currentIndex() != self.sizeLast:
@@ -131,6 +137,7 @@ class MyMainWindow(VCPMainWindow):
             self.sizeMapper.toFirst()
         self.drillSizeInit()
         self.threadSizeCalc()
+        self.numPassesCalc()
 
     def threadSizeBack(self):
         if self.sizeMapper.currentIndex() != 0:
@@ -139,11 +146,12 @@ class MyMainWindow(VCPMainWindow):
             self.sizeMapper.toLast()
         self.drillSizeInit()
         self.threadSizeCalc()
+        self.numPassesCalc()
 
     def threadSizeCalc(self):
         # PDO calculations
         threadMajorDia = float(self.threadMajorDiaLbl.text())
-        drillDia = float(self.drillDiaLbl.text())
+        drillDia = float(self.holeDiaSb.value())
         standardPDO = threadMajorDia - drillDia
         self.sptmThreadingPDOLbl.setText('{:.04f}'.format(standardPDO))
         # Actual thread height = 1/2 PDO
@@ -200,7 +208,7 @@ class MyMainWindow(VCPMainWindow):
         self.sptmCalc()
 
     def sptmCalc(self):
-        drillDiameter = float(self.drillDiaLbl.text())
+        drillDiameter = float(self.holeDiaSb.value())
         sptmCutterDia = float(self.sptmDiaLbl.text())
         if sptmCutterDia < drillDiameter:
             self.sptmDiaOkLbl.setText('Ok')
@@ -209,30 +217,6 @@ class MyMainWindow(VCPMainWindow):
         sptmNeckDia = float(self.sptmNeckDiaLbl.text())
         self.sptmTipHeight = sptmCutterDia - sptmNeckDia
         self.sptmTipHeightLbl.setText('{:.4f}'.format(self.sptmTipHeight))
-
-    def testSql(self):
-        self.testQuery = QSqlQuery()
-        self.testQuery.prepare("SELECT type, size, dia FROM drills WHERE dia >= :min \
-            AND dia <= :max")
-        self.testQuery.bindValue(":min", self.minMinorDiaLbl.text())
-        self.testQuery.bindValue(":max", self.maxMinorDiaLbl.text())
-        if self.testQuery.exec_():
-            self.testQuery.first()
-            #print('at {}'.format(testQuery.at()))
-            #print('{} {} {}'.format(testQuery.value(0),testQuery.value(1),testQuery.value(2)))
-            self.testLbl.setText('Dia {}'.format(self.testQuery.value(2)))
-        else:
-            self.testLbl.setText("Error: ", self.testQuery.lastError().text())
-
-    def testFwd(self):
-        self.testQuery.next()
-        self.testLbl.setText('{} {} {}'.format(self.testQuery.value(0), \
-        self.testQuery.value(1),self.testQuery.value(2)))
-
-    def testBack(self):
-        self.testQuery.previous()
-        self.testLbl.setText('{} {} {}'.format(self.testQuery.value(0), \
-        self.testQuery.value(1),self.testQuery.value(2)))
 
     def drillSizeInit(self):
         self.drillMapper = QDataWidgetMapper(self)
@@ -249,6 +233,13 @@ class MyMainWindow(VCPMainWindow):
         self.drillMapper.toLast()
         self.drillLast = self.drillMapper.currentIndex()
         self.drillMapper.toFirst()
+        # setup hole dia spinbox
+        self.holeDiaSb.setMinimum(float(self.minMinorDiaLbl.text()))
+        self.holeDiaSb.setMaximum(float(self.maxMinorDiaLbl.text()))
+        # setup pitch dia spinbox
+        self.pitchDiaSb.setMinimum(float(self.minPitchDiaLbl.text()))
+        self.pitchDiaSb.setMaximum(float(self.maxPitchDiaLbl.text()))
+
         self.sptmCalc()
         self.threadPercent()
 
@@ -274,9 +265,142 @@ class MyMainWindow(VCPMainWindow):
         majorDia = float(self.threadMajorDiaLbl.text())
         minorDia = float(self.drillDiaLbl.text())
         # note for metric convert to TPI
-        threadPitch = float(self.threadPitchLbl.text())
+        threadPitch = float(self.threadTPILbl.text())
         threadEngagement = ((majorDia - minorDia) * threadPitch) / 0.01299
         self.threadPercentLbl.setText('{:.0f}%'.format(threadEngagement))
+
+    def numPassesCalc(self):
+        majorDia = float(self.threadMajorDiaLbl.text())
+        minorDia = float(self.holeDiaSb.value())
+        threadDepth = (majorDia - minorDia)
+        if self.numPassesSP.value() == 1:
+            self.passDiaLbl_0.setText('{:.4f}'.format(majorDia))
+            self.passDiaLbl_1.setText('')
+            self.passDiaLbl_2.setText('')
+            self.passDiaLbl_3.setText('')
+
+            self.passPercentLbl_1.setText('100%')
+            self.passPercentLbl_2.setText('')
+            self.passPercentLbl_3.setText('')
+            self.passPercentLbl_4.setText('')
+
+        if self.numPassesSP.value() == 2:
+
+            self.passDiaLbl_0.setText('{:.4f}'.format(minorDia \
+                + (threadDepth * 0.65)))
+            self.passDiaLbl_1.setText('{:.4f}'.format(majorDia))
+            self.passDiaLbl_2.setText('')
+            self.passDiaLbl_3.setText('')
+
+            self.passPercentLbl_1.setText('65%')
+            self.passPercentLbl_2.setText('35%')
+            self.passPercentLbl_3.setText('')
+            self.passPercentLbl_4.setText('')
+
+        if self.numPassesSP.value() == 3:
+            self.passDiaLbl_0.setText('{:.4f}'.format(minorDia \
+                + (threadDepth * 0.50)))
+            self.passDiaLbl_1.setText('{:.4f}'.format(minorDia \
+                + (threadDepth * 0.80)))
+            self.passDiaLbl_2.setText('{:.4f}'.format(majorDia))
+            self.passDiaLbl_3.setText('')
+
+            self.passPercentLbl_1.setText('50%')
+            self.passPercentLbl_2.setText('30%')
+            self.passPercentLbl_3.setText('20%')
+            self.passPercentLbl_4.setText('')
+
+        if self.numPassesSP.value() == 4:
+            self.passDiaLbl_0.setText('{:.4f}'.format(minorDia \
+                + (threadDepth * 0.40)))
+            self.passDiaLbl_1.setText('{:.4f}'.format(minorDia \
+                + (threadDepth * 0.67)))
+            self.passDiaLbl_2.setText('{:.4f}'.format(minorDia \
+                + (threadDepth * 0.87)))
+            self.passDiaLbl_3.setText('{:.4f}'.format(majorDia))
+
+            self.passPercentLbl_1.setText('40%')
+            self.passPercentLbl_2.setText('27%')
+            self.passPercentLbl_3.setText('20%')
+            self.passPercentLbl_4.setText('13%')
+
+    def holeDiaCalc(self):
+        majorDia = float(self.threadMajorDiaLbl.text())
+        minorDia = self.holeDiaSb.value()
+        # note for metric convert to TPI
+        threadPitch = float(self.threadTPILbl.text())
+        threadEngagement = ((majorDia - minorDia) * threadPitch) / 0.01299
+        self.threadPerLbl.setText('{:.1f}%'.format(threadEngagement))
+
+
+    def genGcode(self):
+        # make sure your using the hole size as the starting point
+        self.gcodeText.setPlainText("; JT's Thread Mill Wizard")
+        self.gcodeText.append('F25')
+        self.gcodeText.append('G0 X2 Y2 Z0')
+        self.gcodeText.append('G91')
+        self.gcodeText.append('; Number of thread passes {}' \
+            .format(self.numPassesSP.value()))
+        for i in range(self.numPassesSP.value()):
+            passDiameter = float(getattr(self, 'passDiaLbl_' + str(i)).text())
+            # go to hole bottom
+            threadTPI = float(self.threadTPILbl.text())
+            threadPitch = 1.0 / threadTPI
+            threadBottom = -(threadPitch * (self.threadCountSb.value() + 1))
+            self.gcodeText.append('G0 Z{}'.format(threadBottom))
+            # go to start of lead in arc
+            cutterClearance = 0.020
+            minorDia = float(self.holeDiaSb.value())
+            toolDia = float(self.sptmDiaLbl.text())
+            yStartLeadIn = -((minorDia - (cutterClearance * 2)) - toolDia) / 2
+            self.gcodeText.append('G1 Y{:.4f}'.format(yStartLeadIn))
+            # lead in arc
+            sptmCutterDia = float(self.sptmDiaLbl.text())
+            leadInYEnd = ((passDiameter - sptmCutterDia) / 2) + abs(yStartLeadIn)
+            leadInZEnd = threadPitch / 2
+            leadInJOffset = leadInYEnd / 2
+            self.gcodeText.append('G3 X0.0 Y{:.4f} Z{:.4f} J{:.4f}' \
+                .format(leadInYEnd, leadInZEnd, leadInJOffset))
+            # spiral up
+            threadCount = int(self.threadCountSb.text())
+            finalZ = threadPitch * threadCount
+            jOffset = -(passDiameter - sptmCutterDia) / 2
+            self.gcodeText.append('G3 J{} Z{} P{}'. \
+                format(jOffset, finalZ, threadCount))
+            # lead out arc
+            leadOutYEnd = -((passDiameter - sptmCutterDia) / 2) - abs(yStartLeadIn)
+            leadOutZEnd = threadPitch / 2
+            leadOutJOffset = -(leadInYEnd / 2)
+            self.gcodeText.append('G3 X0.0 Y{:.4f} Z{:.4f} J{:.4f}' \
+                .format(leadOutYEnd, leadOutZEnd, leadOutJOffset))
+            # go to center
+            yCenter = ((minorDia - (cutterClearance * 2)) - toolDia) / 2
+            self.gcodeText.append('G1 Y{:.4f}'.format(yCenter))
+
+        self.gcodeText.append('G90')
+        #self.gcodeText.append('G0 X2 Y2')
+        self.gcodeText.append('M2')
+
+    def linearFeedCalc(self):
+        # Internal Threads
+        # Linear feed * ((Major thread dia - Tool dia) / Major thread dia)
+        minorDia = float(self.holeDiaSb.value())
+        majorDia = float(self.threadMajorDiaLbl.text())
+        cutterDia = float(self.sptmDiaLbl.text())
+        linearFeed = self.linearFeedSb.value()
+        circularFeed = ((majorDia - cutterDia) / majorDia) * linearFeed
+        self.circularFeedLbl.setText('{:.1f}'.format(circularFeed))
+
+    def testFwd(self):
+        self.gcodeText.append('line du')
+
+    def testBack(self):
+        pass
+
+    def saveFile(self):
+        ngcFile = os.path.join(os.getenv("HOME"), 'linuxcnc/nc_files', 'jt.ngc')
+        with open(ngcFile, 'w') as f:
+           f.write(str(self.gcodeText.toPlainText()))
 
     """ 
     def appendSPTM(parent):
